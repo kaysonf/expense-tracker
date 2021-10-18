ed369ded281ff51af99cdd251d72159968417563
kayson <kaysonf94@gmail.com>
2021-10-18 16:40:46 +0800

    removed unused import

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -9,7 +9,6 @@ import { User } from './entities/user.entity';
 import { CreateUserDto } from './dto/create-user.dto';
 import { QueryFailedError } from 'typeorm';
 import { UpdateUserDto } from './dto/update-user.dto';
-import e from 'express';
 
 @Injectable()
 export class UsersService {
